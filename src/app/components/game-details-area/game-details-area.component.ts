3d6b5052e532686409f3929f8db1f275bc9185e7
dimitarspiroski <dimitarspiroski997@gmail.com>
2023-08-06 23:21:48 +0200

    Hex rework, material added, custom theme

--- src/app/components/game-details-area/game-details-area.component.ts
+++ src/app/components/game-details-area/game-details-area.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'game-details-area',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './game-details-area.component.html',
   styleUrls: ['./game-details-area.component.scss'],
 })
